1b6cc725a66da58cedaebfb8e55999d1b6ecfa72
ManaraMarcelo <161091755+ManaraMarcelo@users.noreply.github.com>
2025-03-26 12:14:04 -0300

    Update monitor_nginx.sh

--- scripts/monitor_nginx.sh
+++ scripts/monitor_nginx.sh
@@ -3,7 +3,7 @@
       if ! systemctl is-active --quiet nginx; then
           echo "⚠️ Nginx caiu! Reiniciando e enviando notificação..."
           echo "$(TZ=America/Sao_Paulo date '+%Y-%m-%d %H:%M:%S') - O site caiu e foi reiniciado" >> /var/log/nginx_monitoramento.log
-          python3 /<caminho ate seu codigo python>/telegram_notify.py
+          python3 /usr/local/bin/telegram_notify.py
       fi
       sleep 10  # Verifica a cada 10s
   done 
